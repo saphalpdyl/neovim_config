8288d2675cbf0e20648fd233b25d951d4a13150f
saphalpdyl <saphalpdyl@gmail.com>
2023-12-16 10:00:53 +0545

    UPDATE: Added more parser in Treesitter config

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "help", "query" , "typescript" , 'javascript' },
+  ensure_installed = { "c", "lua", "vim", "query" , "typescript" , "javascript", "json", "python","sql" },
 
   sync_install = false,
 
