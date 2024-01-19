99b0810acf3c061a5e8a0bdb997d65a70f12d1d0
saphalpdyl <saphalpdyl@gmail.com>
2024-01-19 23:08:39 +0545

    Added indent lines & animations

--- lua/saphal/plugins/indent.lua
+++ lua/saphal/plugins/indent.lua
@@ -1,5 +1,59 @@
 return {
   {
-
+    "lukas-reineke/indent-blankline.nvim",
+    lazy = false,
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = { enabled = false }
+    },
+    main = "ibl",
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      }
+    },
+  },
+  {
+    "echasnovski/mini.indentscope",
+    version = false,
+    opts = {
+      symbol = "│",
+      options = {
+        try_as_border = true
+      }
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end
+      })
+    end
   }
 }
