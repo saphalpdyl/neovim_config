f63ad5f607cdffa676dd2bbee4669ab7d8bb2f55
saphalpdyl <saphalpdyl@gmail.com>
2024-01-20 17:11:53 +0545

    Added some gitsigns keymaps

--- lua/saphal/plugins/gitsigns.lua
+++ lua/saphal/plugins/gitsigns.lua
@@ -45,5 +45,31 @@ return {
     yadm = {
       enable = false
     },
-  }
+  },
+  init = function()
+    local gs = require("gitsigns")
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode,l,r,opts)
+    end
+
+    map('n','<leader>hs', gs.stage_hunk, { desc = "Stage current hunk" })
+    map('n','<leader>hr', gs.reset_hunk, { desc = "Reset current hunk" })
+    map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "Stage visually selected lines" })
+    map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = "Reset visually selected lines" })
+    map('n', '<leader>hS', gs.stage_buffer, { desc = "Stage entire buffer" })
+    map('n', '<leader>hu', gs.undo_stage_hunk, { desc = "Undo last hunk staging" })
+    map('n', '<leader>hR', gs.reset_buffer, { desc = "Reset entire buffer" })
+    map('n', '<leader>hp', gs.preview_hunk, { desc = "Preview current hunk changes" })
+    map('n', '<leader>hb', function() gs.blame_line {full=true} end, { desc = "Show blame for current line" })
+    map('n', '<leader>htb', gs.toggle_current_line_blame, { desc = "Toggle blame for current line" })
+    map('n', '<leader>hd', gs.diffthis, { desc = "Diff current hunk" })
+    map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = "Diff current hunk against its base" })
+    map('n', '<leader>htd', gs.toggle_deleted, { desc = "Toggle visibility of deleted lines" })
+    map('n', '<leader>hj', gs.next_hunk, {desc = "Go to next hunk" })
+    map('n', '<leader>hk', gs.prev_hunk, {desc = "Go to previous hunk" })
+
+  end
 }
--- lua/saphal/plugins/which_key.lua
+++ lua/saphal/plugins/which_key.lua
@@ -2,5 +2,12 @@ return {
   "folke/which-key.nvim",
   config = function()
     require("which-key").setup()
+    require("which-key").register({
+      ["<leader>"] = {
+        h = {
+          name = "GitSigns"
+        }
+      }
+    })
   end
 }
